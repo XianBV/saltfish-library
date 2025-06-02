5b992ea96bc7ebf626d1b1a9393a474480b862c8
XianBV <yuliya.xian@gmail.com>
2025-06-02 12:10:05 +0500

    Добавлены заглушки недостающих модулей

--- apps/api/src/prisma/prisma.module.ts
+++ apps/api/src/prisma/prisma.module.ts
@@ -7,14 +7,3 @@ import { PrismaService } from './prisma.service';
   exports: [PrismaService],
 })
 export class PrismaModule {}
-
-// apps/api/src/prisma/prisma.service.ts
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-
-@Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
-  }
-}
